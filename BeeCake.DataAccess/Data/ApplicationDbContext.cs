77b7c7276b9c80669fc0cbd44ed7f53c63ee654a
agnesaselmani03 <agnesaselmani03@gmail.com>
2024-03-01 18:56:55 +0100

    Added DbSets

---
+++ BeeCake.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BeeCake.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BeeCakeWeb.DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<CakeType> CakeType { get; set; }
+        public DbSet<MenuItem> MenuItem { get; set; }
+    }
+}
