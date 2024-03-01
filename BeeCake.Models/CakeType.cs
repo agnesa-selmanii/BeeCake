e43220764e29243e3436a8f331b52f811627591b
agnesaselmani03 <agnesaselmani03@gmail.com>
2024-03-01 17:17:50 +0100

    Added models

---
+++ BeeCake.Models/CakeType.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BeeCake.Models
+{
+    public class CakeType
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
